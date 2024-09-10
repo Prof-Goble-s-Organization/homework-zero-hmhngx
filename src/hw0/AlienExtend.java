1eee8e2f9a6763b8c414b5930f2a5cf68cdd9a8e
Harrison Nguyen <nhminhhung05@gmail.com>
2024-09-10 00:26:25 -0400

    hw0 finished

---
+++ src/hw0/AlienExtend.java
@@ -0,0 +1,36 @@
+package hw0;
+
+public class AlienExtend extends SpaceAlien implements ZapsWithSlime{
+
+    private int spaceships;
+
+    public AlienExtend(int spaceships, String homePlanet, double distanceFromEarth) {
+        super(homePlanet, distanceFromEarth);
+        this.spaceships = spaceships;
+    }
+
+    @Override
+    public String doSlimeZap(int numHumans) {
+        // TODO Auto-generated method stub
+        return("We are going turn " + numHumans + " into slime.");
+    }
+
+    @Override
+    public String slimeWholeEarth() {
+        // TODO Auto-generated method stub
+        return("We will cover the whole Earth in slime with our " + spaceships + " spaceships.");
+    }
+    
+    @Override
+    public void doGreeting() {
+        System.out.println("We are aliens who are coming with " + spaceships + " spaceships.");
+    }
+
+    public static void main(String[] args) {
+        AlienExtend ae = new AlienExtend(10, "Gaytopia", 1000000);
+        ae.doGreeting();
+        ae.slimeWholeEarth();
+        ae.doSlimeZap(15);
+    }
+    
+}
